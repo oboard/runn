8a48c9e46db8dedb97b8d8acd494bc1b1c1f8738
oboard <oboard@outlook.com>
2019-06-27 07:50:48 +0800

    添加选曲列表

--- 跳跃的数字/app/src/main/java/com/oboard/runn/MainActivity.java
+++ 跳跃的数字/app/src/main/java/com/oboard/runn/MainActivity.java
@@ -8,10 +8,18 @@ import android.view.WindowManager;
 import android.view.Window;
 import android.view.View;
 import android.os.Build;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.util.AttributeSet;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
 
 	ImageView startImage;
+	LinearLayout lay;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_start);
 		
 		startImage = findViewById(R.id.startimage);
+		
 		ValueAnimator ani = ValueAnimator.ofFloat(0.01f, 0.8f,0);
 
 		ani.setDuration(2000).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -27,7 +36,8 @@ public class MainActivity extends Activity {
 				float a = animaion.getAnimatedValue();
 				startImage.setAlpha(a);
 				if(a==0) {
-					setContentView(R.layout.activity_main);
+					loadmain();
+					
 				}
 			}
 		});
@@ -36,5 +46,43 @@ public class MainActivity extends Activity {
 		
 		
     }
-    
+	public void loadmain() {
+		setContentView(R.layout.activity_main);
+		lay = findViewById(R.id.activitymainLinearLayout);
+		loaddisk();
+		loaddisk();
+		loaddisk();
+		loaddisk();
+	}
+    public void loaddisk() {
+		lay.addView(new DiskView(this));
+	}
+	
+	class DiskView extends FrameLayout {
+		private ImageView mImg;
+		
+		private TextView mTitleTv;
+
+		public DiskView(Context context) {
+			super(context);
+
+			// 加载布局
+			LayoutInflater.from(context).inflate(R.layout.view_disk, this);
+
+			// 获取控件
+			mImg = findViewById(R.id.viewdiskImageView);
+			mTitleTv = findViewById(R.id.viewdiskTextView);
+
+		}
+
+		// 点击事件
+		public void setListener(OnClickListener listener) {
+			mTitleTv.setOnClickListener(listener);
+		}
+
+		// 设置标题的方法
+		public void setTitleText(String title) {
+			mTitleTv.setText(title);
+		}
+	}
 }

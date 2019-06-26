31d9ef9259edd1b9904048d5b484e38d796b21c7
oboard <oboard@outlook.com>
2019-06-26 20:17:58 +0800

    添加主页

--- 跳跃的数字/app/src/main/java/com/oboard/runn/MainActivity.java
+++ 跳跃的数字/app/src/main/java/com/oboard/runn/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.animation.ValueAnimator;
 import android.widget.ImageView;
+import android.view.WindowManager;
+import android.view.Window;
+import android.view.View;
+import android.os.Build;
 
 public class MainActivity extends Activity {
 
@@ -12,18 +16,24 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+		
+		setContentView(R.layout.activity_start);
 		
 		startImage = findViewById(R.id.startimage);
-		ValueAnimator ani = ValueAnimator.ofFloat(0, 0.8f,0);
+		ValueAnimator ani = ValueAnimator.ofFloat(0.01f, 0.8f,0);
 
-		ani.setDuration(5000).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+		ani.setDuration(2000).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 			public void onAnimationUpdate(ValueAnimator animaion) {
 				float a = animaion.getAnimatedValue();
 				startImage.setAlpha(a);
+				if(a==0) {
+					setContentView(R.layout.activity_main);
+				}
 			}
 		});
 		ani.start();
+		
+		
 		
     }
     

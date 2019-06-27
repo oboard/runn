899e7fb6dba4b208a8d6a0b0e2de49b8fb0de642
oboard <oboard@outlook.com>
2019-06-27 09:56:28 +0800

    添加滚动效果

--- 跳跃的数字/app/src/main/java/com/oboard/runn/MainActivity.java
+++ 跳跃的数字/app/src/main/java/com/oboard/runn/MainActivity.java
@@ -15,52 +15,80 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.FrameLayout;
+import android.widget.ScrollView;
+import android.view.View.OnTouchListener;
+import android.view.MotionEvent;
+import android.os.Debug;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MainActivity extends Activity {
 
 	ImageView startImage;
 	LinearLayout lay;
+	ScrollView scr;
+	ArrayList<DiskView> dv = new ArrayList<DiskView>();
+
 	
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_start);
-		
+
 		startImage = findViewById(R.id.startimage);
-		
-		ValueAnimator ani = ValueAnimator.ofFloat(0.01f, 0.8f,0);
+
+		ValueAnimator ani = ValueAnimator.ofFloat(0.01f, 0.8f, 0);
 
 		ani.setDuration(2000).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 			public void onAnimationUpdate(ValueAnimator animaion) {
 				float a = animaion.getAnimatedValue();
 				startImage.setAlpha(a);
-				if(a==0) {
+				if (a == 0) {
 					loadmain();
-					
+
 				}
 			}
 		});
 		ani.start();
-		
-		
-		
+
+
+
     }
 	public void loadmain() {
 		setContentView(R.layout.activity_main);
 		lay = findViewById(R.id.activitymainLinearLayout);
+		scr = findViewById(R.id.activitymainScrollView);
+		scr.setOnTouchListener(new OnTouchListener() {
+			public boolean onTouch(View view , MotionEvent me) {
+			 	int sy = view.getScrollY();
+				Iterator it1 = dv.iterator();
+				while (it1.hasNext()) {
+					DiskView d = (DiskView)it1.next();
+					float a = 1 - Math.abs(scr.getHeight() / 2 - (d.getY() - sy + d.getHeight() / 2)) / scr.getHeight();
+					d.setScaleX(a);
+					d.setScaleY(a);
+					d.setAlpha(a);
+				}
+				return false;
+			}
+		});
+		
 		loaddisk();
 		loaddisk();
 		loaddisk();
 		loaddisk();
 	}
     public void loaddisk() {
-		lay.addView(new DiskView(this));
+		DiskView it = new DiskView(this);
+		dv.add(it);
+		lay.addView(it);
 	}
-	
+
 	class DiskView extends FrameLayout {
 		private ImageView mImg;
-		
+
 		private TextView mTitleTv;
 
 		public DiskView(Context context) {
